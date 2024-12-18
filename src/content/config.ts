445d5782596c393d5cc4bd2062989189e89fd763
TomasBankauskas <tbdizainas@gmail.com>
2024-12-18 20:24:06 +0200

    updated config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 const guides = defineCollection({
     schema: z.object({
-        title: z.string().optional(),
+        title: z.string(),
     })
 });
 
